839031e1dbfc454389c713504bd535faa6ea8647
Tyler Hogsett <tyhogset@microsoft.com>
2018-02-21 20:49:01 -0800

    Add project files.

---
+++ XmlServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace XmlServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Map(string.Empty, Handler))
+                .Build();
+
+            host.Run();
+        }
+
+        private static void Handler(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                var xmlContents = File.ReadAllText("./idp_metadata.xml");
+                await context.Response.WriteAsync(xmlContents);
+            });
+        }
+    }
+}
